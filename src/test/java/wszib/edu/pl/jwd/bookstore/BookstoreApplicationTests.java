3f246e6d687cda4b72cf078fb0eee2d356bde9d1
wizurek <wizurek@student.wszib.edu.pl>
2020-07-09 15:40:13 +0200

    Initial commit

---
+++ src/test/java/wszib/edu/pl/jwd/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package wszib.edu.pl.jwd.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
